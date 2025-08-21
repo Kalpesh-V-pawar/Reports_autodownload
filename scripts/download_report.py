a9871573e018212802090fe9e93bd58b1749ede0
Kalpesh-V-pawar <Kalpeshpawar010401@gmail.com>
2025-08-21 13:47:51 +0530

    Update download_report.py

--- scripts/download_report.py
+++ scripts/download_report.py
@@ -11,7 +11,7 @@ def run():
         page = context.new_page()
 
         # 1. Login
-        page.goto("https://your-site.com")  # <-- replace with real URL
+        page.goto("https://sopan.kpix.io/login")  # <-- replace with real URL
         page.fill('xpath=//*[@id="exampleInputEmail"]', USERNAME)
         page.fill('xpath=//*[@id="exampleInputPassword"]', PASSWORD)
         page.click('xpath=//*[@id="routeWrapperDiv"]/app-rdm-login/div/div/div/div[3]/div[1]/form/div[2]/button')
